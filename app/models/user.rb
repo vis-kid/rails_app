bc0b47d0c8f75ff7e4c2cd739c9d533375bfe2ce
ed wassermann <visavis.kid@gmail.com>
2012-12-18 00:17:41 -0800

    finish the sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX  },
                     uniqueness: { case_sensitive: false }
 
-  validates :password, presence: true
-  validates :password_confirmation, presence: true,
-                                    length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+                                    
 end
